8f2b284970460a93991eedadea24e91cf8fb86cb
4acf <acfree332211@gmail.com>
2024-11-27 11:44:53 -0800

    api skeleton + example swagger headers

---
+++ internal/handlers/animalProject.go
@@ -0,0 +1,115 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+//ANIMALS
+func GetAnimalDocs(c *gin.Context) {
+	
+}
+
+func GetAnimal(c *gin.Context) {
+	
+}
+
+func AddAnimal(c *gin.Context) {
+	
+}
+
+func UpdateAnimal(c *gin.Context) {
+	
+}
+
+func UpdateRateOfGain(c *gin.Context) {
+	
+}
+
+//FEED
+func GetFeedDocs(c *gin.Context) {
+	
+}
+
+func GetFeed(c *gin.Context) {
+	
+}
+
+func AddFeed(c *gin.Context) {
+	
+}
+
+func AddFeedNoForm(c *gin.Context) {
+	
+}
+
+func UpdateFeed(c *gin.Context) {
+	
+}
+
+//FEED PURCHASE
+func GetFeedPurchaseDocs(c *gin.Context) {
+
+}
+
+func GetFeedPurchase(c *gin.Context) {
+
+}
+
+func AddFeedPurchase(c *gin.Context) {
+
+}
+
+func UpdateFeedPurchase(c *gin.Context) {
+
+}
+
+//DAILY FEED
+func GetDailyFeedDocs(c *gin.Context) {
+
+} 
+
+func GetDailyFeed(c *gin.Context) {
+
+}
+
+func AddDailyFeed(c *gin.Context) {
+
+}
+
+func UpdateDailyFeed(c *gin.Context) {
+
+}
+
+//EXPENSES
+func GetExpenseDocs(c *gin.Context) {
+
+}
+
+func GetExpense(c *gin.Context) {
+
+}
+
+func AddExpense(c *gin.Context) {
+
+}
+
+//SUPPLY INVENTORY
+func GetSupplyDocs(c *gin.Context) {
+
+}
+
+func GetSupply(c *gin.Context) {
+
+}
+
+func AddSupply(c *gin.Context) {
+
+}
+
+func UpdateSupply(c *gin.Context) {
+
+}
+
+func DeleteSupply(c *gin.Context) {
+
+}
---
+++ internal/handlers/project.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// GetCurrentProjects godoc
+// @Summary 
+// @Description 
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Success 200 
+// @Router /projects [get]
+func GetCurrentProjects(c *gin.Context) {
+
+}
+
+// GetProjects godoc
+// @Summary 
+// @Description 
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Success 200 
+// @Router /project [get]
+func GetProjects(c *gin.Context) {
+	
+}
+
+// GetProject godoc
+// @Summary 
+// @Description 
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Success 200 
+// @Router /project/{projectId} [get]
+func GetProject(c *gin.Context) {
+	
+}
+
+// AddProject godoc
+// @Summary 
+// @Description 
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Success 200 
+// @Router /project [post]
+func AddProject(c *gin.Context) {
+	
+}
+
+// UpdateProject godoc
+// @Summary 
+// @Description 
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Success 200 
+// @Router /project/{projectId} [put]
+func UpdateProject(c *gin.Context) {
+	
+}
