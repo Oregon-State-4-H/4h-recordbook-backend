1a1653182c5716c92a35e36c022bd6dcdc0da57b
BP <bp@bps-server.xyz>
2024-12-04 01:01:14 -0800

    Added some boilerplate code from docs

--- config/config.go
+++ config/config.go
@@ -20,6 +20,14 @@ type Database struct {
 	DatabaseName	string
 }
 
+type Auth0 struct {
+	Secret         string
+	ClientID       string
+	ClientSecret   string
+	BaseURL        string
+	IssuerBaseURL  string
+}
+
 func (c *Config) Read() {
 
 	err := godotenv.Load()
@@ -31,9 +39,21 @@ func (c *Config) Read() {
 	if uri == "" {
 		log.Fatal("Set 'MONGODB_URI' environment variable")
 	}
+	auth0_secret = os.Getenv("AUTH0_SECRET")
+	auth0_clientID = os.Getenv("AUTH0_BASE_URL")
+	auth0_clientSecret = os.Getenv("AUTH0_ISSUER_BASE_URL")
+	auth0_baseURL = os.Getenv("AUTH0_CLIENT_ID")
+	auth0_issuerBaseURL = os.Getenv("AUTH0_CLIENT_SECRET")
 
 	c.Server.Port = ":8000"
 	c.Database.Uri = uri
 	c.Database.DatabaseName = "demo"
-
+	
+	c.Auth0 = Auth0{
+		auth0_secret,
+		auth0_clientID,
+                auth0_clientSecret,
+                auth0_baseURL,
+                auth0_issuerBaseURL
+	}
 }
